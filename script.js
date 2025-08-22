88dc7ea60fc54f1597ddb7d27cef5a1f93f7c931
Ayush-Rajput123 <131739732+Ayush-Rajput123@users.noreply.github.com>
2025-08-22 17:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
+const Prices = document.getElementsByClassName("price");
+const total=document.getElementById("total")
 const getSum = () => {
-//Add your code here
-  
+  let totalsum=0
+	for(let val of Prices){
+	  totalsum +=parseFloat(val.textContent) || 0
+  }
+	total.textContent=totalsum
+	return totalsum
 };
-
 getSumBtn.addEventListener("click", getSum);
 
